484bbc7c2ec893df20d0d77adb9a751045ab2e4a
Muhammad Azhar <azhar.cs@outlook.com>
2020-01-19 22:34:01 -0600

    added code

---
+++ Core3.1WebAPIJWT/CoreAPIJWT/Controllers/AuthenticateController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CoreAPIJWT.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CoreAPIJWT.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class AuthenticateController : ControllerBase
+    {
+        private readonly JWTAppSettings _appSettings;
+        public AuthenticateController(IOptions<JWTAppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Post()
+        {
+            var user = new User();
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.key);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, "USER NAME"),
+                    new Claim(ClaimTypes.DateOfBirth, "01/01/01")
+
+                }),          
+            
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            return Ok(user);
+        }
+    }
+}
---
+++ Core3.1WebAPIJWT/CoreAPIJWT/Model/JWTAppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAPIJWT.Model
+{
+    public class JWTAppSettings
+    {
+        public string key { get; set; }
+    }
+}
